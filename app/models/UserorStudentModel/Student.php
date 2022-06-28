5f772af0b8297825fe0d4e9db9094778731db81d
zedsoul <1484908116@qq.com>
2022-06-28 15:48:15 +0800

    zlc

--- app/models/UserorStudentModel/Student.php
+++ app/models/UserorStudentModel/Student.php
@@ -3,8 +3,23 @@
 namespace App\models\UserorStudentModel;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Student extends Model
 {
     //
+    protected $table="student";
+    const CREATED_AT = 'created_at';
+    const UPDATED_AT = 'updated_at';
+    protected $primaryKey="id";
+    public $timestamps = true;
+
+
+
+    public  static  function zlcSelectStudent($student_id){
+        $res =DB::table("student")->where("student_num",$student_id)->select(
+            "student_name","student_num","student_level", "student_year","student_spec","student_class"
+        )->get();
+        return $res;
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,10 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::middleware('jwt.auth')->group(function () {
+    Route::post("selectstudent",'informationFindController@zlcSelectstudent');//查看个人学生信息
+    Route::post("danbai","PendulumController@zlcDanbai");//单摆
+    Route::post("js","PendulumController@zlcJs");//单摆计算题
+
 });
